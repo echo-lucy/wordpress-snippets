dc43c932fe32879851b5f64b1f0136c17999bce6
echo-lucy <121934136+echo-lucy@users.noreply.github.com>
2023-02-07 21:14:43 +0000

    Update remove-title-attribute.php

--- remove-title-attribute.php
+++ remove-title-attribute.php
@@ -1,5 +1,7 @@
 //Remove title attribute from nav
+<?php
 add_filter( 'wp_nav_menu', 'conversions_menu_notitle' );
 function conversions_menu_notitle( $menu ){
   return $menu = preg_replace('/ title=\"(.*?)\"/', '', $menu );
 }
+?>
