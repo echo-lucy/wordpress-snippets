ac3727c2ab2b6ae6b3b5a51aae301c5288c98a6b
echo-lucy <121934136+echo-lucy@users.noreply.github.com>
2023-08-10 12:07:05 +0100

    Update product-schema.php

--- product-schema.php
+++ product-schema.php
@@ -1,4 +1,5 @@
 <?php
+//Woocommerce Product Schema 
 add_action('wp_footer', 'lk_product_schema');
 
 function lk_product_schema()
@@ -26,4 +27,42 @@ function lk_product_schema()
         </script>
 <?php }
 } 
+?>
+
+
+<?php 
+//Woocommerce Alternate Method Product Schema 
+function add_product_schema() {
+    if (is_product()) {
+        global $product;
+
+        $product_name = get_the_title();
+        $product_price = $product->get_price();
+        $product_description = wp_strip_all_tags(get_the_excerpt()); // Strip HTML tags
+        $product_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');
+
+        // Get product availability
+        $availability = 'http://schema.org/OutOfStock';
+        if ($product->is_in_stock()) {
+            $availability = 'http://schema.org/InStock';
+        }
+
+        $product_schema = array(
+            '@context' => 'http://schema.org',
+            '@type' => 'Product',
+            'name' => $product_name,
+            'image' => $product_image[0],
+            'description' => $product_description,
+            'offers' => array(
+                '@type' => 'Offer',
+                'price' => $product_price,
+                'priceCurrency' => 'GBP',
+                'availability' => $availability,
+            ),
+        );
+
+        echo '<script type="application/ld+json">' . json_encode($product_schema) . '</script>';
+    }
+}
+add_action('wp_head', 'add_product_schema');
 ?>
